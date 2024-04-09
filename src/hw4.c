545e08d3b4a2b005b0bcbaa9ff5fb70dd755fac1
andy3200 <149091611+andy3200@users.noreply.github.com>
2024-04-09 21:35:34 +0000

    done with check pawn

--- src/hw4.c
+++ src/hw4.c
@@ -1,7 +1,25 @@
 #include "hw4.h"
 
 void initialize_game(ChessGame *game) {
-    (void)game;
+    game->capturedCount = 0;
+    game->moveCount = 0;
+    game->currentPlayer = WHITE_PLAYER;
+    char initial_board[8][8] = {
+        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {'.', '.', '.', '.', '.', '.', '.', '.'},
+        {'.', '.', '.', '.', '.', '.', '.', '.'},
+        {'.', '.', '.', '.', '.', '.', '.', '.'},
+        {'.', '.', '.', '.', '.', '.', '.', '.'},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+    };
+}
+/// @brief checks whether the piece is white or not 
+/// @param c the piece  
+/// @return  return 1 if true return 0 if black 
+int check_white(char c) {
+    return (c >= 'A' && c <= 'Z');
 }
 
 void chessboard_to_fen(char fen[], ChessGame *game) {
@@ -10,13 +28,81 @@ void chessboard_to_fen(char fen[], ChessGame *game) {
 }
 
 bool is_valid_pawn_move(char piece, int src_row, int src_col, int dest_row, int dest_col, ChessGame *game) {
-    (void)piece;
-    (void)src_row;
-    (void)src_col;
-    (void)dest_row;
-    (void)dest_col;
-    (void)game;
-    return false;
+    int move_direction_vertical;
+    int move_direction_horizontal;
+    int eating = 0;
+    move_direction_horizontal = dest_col - src_col; // - means left + means right 
+    move_direction_vertical = dest_row - src_row; // - means moving up. + means moving down 
+    int net_movement_vertical = abs(move_direction_vertical);
+    int net_movement_horizontal = abs(move_direction_horizontal);
+    
+    if((net_movement_horizontal == 0) && (net_movement_vertical) ==0){//not moving at all 
+        return false; 
+    }
+
+    if((game->currentPlayer == WHITE_PLAYER) && (move_direction_vertical > 0)){ //white but moving down 
+        return false ;
+    }
+    if((game->currentPlayer == BLACK_PLAYER) && (move_direction_vertical < 0)){ //black but moving up 
+        return false ;
+    }
+    if((net_movement_horizontal != 0)){ // the pawn is eating something. 
+        if(net_movement_vertical != 1){ //move horizontal but not vertical
+            return false;
+        }
+        if(net_movement_horizontal > 1){ //moving horizontally more than 1 space left or right 
+            return false;
+        }
+        eating = 1;
+    }
+    if(net_movement_vertical == 2){ //move up or down by 2 
+        if(((game->currentPlayer == WHITE_PLAYER) && (src_row == 6)) ||((game->currentPlayer == BLACK_PLAYER) && (src_row == 1)) ){
+            //valid, now check if there is something in between
+            if(game->currentPlayer == WHITE_PLAYER){
+                if(game->chessboard[dest_row+1][dest_col] != '.'){ //thing in between is not empty 
+                    return false;
+                }else if(game->chessboard[dest_row][dest_col] != '.'){ //destination is not empty 
+                    return false; 
+                }
+                else{//valid move 
+                    return true; 
+                }
+            }
+            if(game->currentPlayer == BLACK_PLAYER){
+                if(game->chessboard[dest_row-1][dest_col] != '.'){
+                    return false;
+                }else{
+                    return true; 
+                }
+            }
+
+
+        }else if(net_movement_horizontal != 0){ //move 2 vertically but also moving to left or right 
+            return false; 
+        }
+        else{
+            return false; // moved 2 but not in starting position 
+        }
+    }
+    if(net_movement_vertical> 2){ // cant move more than 2 
+        return false;
+    }
+    if(eating){
+        if( (game->currentPlayer == WHITE_PLAYER) && (check_white(game->chessboard[dest_row][dest_col]) == 1) ){ // white eat white 
+            return false;
+        }
+        else if((game->currentPlayer == BLACK_PLAYER) && (check_white(game->chessboard[dest_row][dest_col]) == 0)){//black eat black
+            return false;
+        }else{
+            return true;
+        }
+    }
+    if(game->chessboard[dest_row][dest_col] != '.'){ //destination not empty 
+        return false;
+    }else{
+        return true; 
+    }
+    
 }
 
 bool is_valid_rook_move(int src_row, int src_col, int dest_row, int dest_col, ChessGame *game) {
